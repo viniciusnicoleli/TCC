8b86decfea01cd91ac8a611fa5519300efdccfe7
Vinicius Nicoleli <viniciusnicoleli@gmail.com>
2023-05-06 12:18:05 -0300

    commit_all

---
+++ src/lightgbm.py
@@ -0,0 +1,161 @@
+import pandas as pd
+import numpy as np
+import os
+import sklearn.pipeline
+import skopt 
+import os, sys
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer
+sys.path.insert(0, os.path.abspath(".."))
+from utilidades.calibration import utilities as ult
+from skopt import BayesSearchCV
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelEncoder, LabelBinarizer, OrdinalEncoder, OneHotEncoder
+from sklearn import set_config
+from sklearn.model_selection import learning_curve, validation_curve
+from lightgbm import LGBMClassifier
+from sklearn.model_selection import StratifiedKFold
+from lightgbm import early_stopping
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.metrics import average_precision_score
+from sklearn.metrics import precision_recall_curve, auc, average_precision_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.impute import SimpleImputer
+from skopt.space import Real, Categorical, Integer
+from skopt import BayesSearchCV
+import time
+
+
+
+class tcc_lgbm():
+    def __init__(self, dataframe : pd.DataFrame, target: str, metric : str = 'average_precision', pipe_final : sklearn.pipeline = None):
+        self.dataframe = dataframe
+        self.target = target
+        self.metric = metric
+        self._pipe_final = pipe_final
+    
+    def fit(self):
+        X, y = ult.splitxy(self.dataframe, self.target)
+        X_train, y_train, X_test, y_test, X_val, y_val = ult.train_test_val(X,y)
+    
+        prep_feat_tuple = ult.create_prep_pipe(self.dataframe, self.target)
+        prep_feat = prep_feat_tuple[0]
+    
+        lists_pandarizer = list(prep_feat_tuple[1]) + list(prep_feat_tuple[2])
+        
+        LGBM = LGBMClassifier(random_state = 42, n_jobs = -1)
+
+        pipe_tuning = Pipeline([
+            ('transformer_prep', prep_feat),
+            ("pandarizer", FunctionTransformer(lambda x: pd.DataFrame(x, columns = lists_pandarizer))),
+            ('estimator', LGBM)
+        ])
+        
+        pipe_prep = Pipeline([
+            ('transformer_prep', prep_feat),
+            ("pandarizer", FunctionTransformer(lambda x: pd.DataFrame(x, columns = lists_pandarizer))),
+        ])
+        pipe_prep.fit(X_train)       
+        
+        cv = StratifiedShuffleSplit(n_splits = 5, test_size = 0.3, random_state = 42)
+        
+        metric = self.metric
+        
+        fit_params = {
+            'eval_metric': metric, 
+            'eval_set': [(X_test, pd.DataFrame(y_test))],
+            'callbacks': [(early_stopping(stopping_rounds = 10, verbose = True))],
+        }        
+        
+        LGBM_search_space = {
+            "estimator__learning_rate": Real(0.001, 0.01, prior = 'log-uniform'),
+            "estimator__n_estimators": Integer(100, 1000),
+            "estimator__class_weight": Categorical(['balanced', None]),
+            "estimator__num_leaves": Integer(32, 256),
+            "estimator__min_child_samples": Integer(100, 1000),
+            "estimator__reg_alpha": Real(0, 100, prior = 'uniform'),
+            "estimator__reg_lambda": Real(10., 200., prior = 'uniform'),
+            "estimator__objective": Categorical(['binary']),
+            "estimator__importance_type":Categorical(['gain']),
+            "estimator__boosting_type": Categorical(['goss'])
+        }    
+        
+        LGBM_bayes_search = BayesSearchCV(pipe_tuning, LGBM_search_space, n_iter = 2, scoring = metric, 
+                                         return_train_score = True, 
+                                         fit_params = fit_params,
+                                         n_jobs = -1, cv = cv, random_state = 42, optimizer_kwargs = {'base_estimator': 'GP'})
+        
+        
+        LGBM_bayes_search.fit(X_train, y_train)        
+        
+        results_cv = pd.DataFrame(LGBM_bayes_search.cv_results_)
+        
+        temp = results_cv[['mean_train_score', 'mean_test_score']]
+        temp['diff'] = temp['mean_test_score'] - temp['mean_train_score']
+        to_go = temp[abs(temp['diff']) < 0.05].sort_values(by = 'mean_test_score', ascending = False).head(1).index
+        
+        params = results_cv.loc[to_go.values[0]]
+        kwargs = params.params   
+        print(kwargs)
+        
+        best_LGBM = LGBMClassifier(random_state = 42, n_jobs = -1, verbose = -1, **kwargs)
+        
+        best_LGBM.fit(pipe_prep.transform(X_train), y_train, early_stopping_rounds = 10, verbose = 20, eval_metric = metric,
+                     eval_set = [(pipe_prep.transform(X_test), y_test)]) 
+        
+        
+        pipe_final = Pipeline(
+        [
+            ('pipe_transformer_prep', pipe_prep),
+            ('pipe_estimator', best_LGBM)
+        ])       
+        
+        self._pipe_final = pipe_final
+        
+    def predict_proba(self, who : str = 'val'):
+        if self._pipe_final is None:
+            return None
+        
+        X_train, y_train, X_test, y_test, X_val, y_val = ult.train_test_val(X,y)        
+        
+        dic = {'val': X_val, 
+              'test': X_test,
+              'train': X_train}
+        y_score = self._pipe_final.predict_proba(dic[who])[:,1]
+        return y_score
+    
+    def get_metric(self, who : str = 'val'):
+        if self._pipe_final is None:
+            return None
+        
+        X, y = ult.splitxy(self.dataframe, self.target)
+        X_train, y_train, X_test, y_test, X_val, y_val = ult.train_test_val(X,y)
+        
+        dic_x = {'val': X_val, 
+              'test': X_test,
+              'train': X_train}
+
+        dic_y = {'val': y_val, 
+              'test': y_test,
+              'train': y_train}
+        
+        
+        y_score = self._pipe_final.predict_proba(dic_x[who])[:,1]
+        average_precision = ult.average_precision_score(dic_y[who], y_score)
+        return average_precision
+    
+    
+    def plot_dist(self):
+        if self._pipe_final is None:
+            return None        
+
+        X, y = ult.splitxy(self.dataframe, self.target)
+        X_train, y_train, X_test, y_test, X_val, y_val = ult.train_test_val(X,y)        
+        
+        y_score_train = self._pipe_final.predict_proba(X_train)[:,1]
+        y_score_val = self._pipe_final.predict_proba(X_val)[:,1]
+        
+        plot_dist(y_train, y_score_train, y_val, y_score_val)
---
+++ utilidades/calibration.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import FeatureUnion
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class utilities():    
+    def splitxy(dataframe : pd.DataFrame, y : str = 'target'):
+        X = dataframe.drop(labels = [y], axis = 1)
+        y = dataframe[y]
+        
+        return(X, y)
+
+
+    def train_test_val(X: pd.DataFrame, y: pd.Series):
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.33, random_state=42, stratify = y)
+        X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.33, random_state=42, stratify = y_train)
+        
+        return(X_train, y_train, X_test, y_test, X_val, y_val)
+
+
+    def create_prep_pipe(dataframe : pd.DataFrame, target_column : str) -> (sklearn.pipeline.FeatureUnion, pd.Series, pd.Series):
+        dataframe = dataframe.drop(labels = [target_column], axis = 1)
+        num_cols = dataframe.select_dtypes(include=[float, int]).columns
+        cat_cols = dataframe.select_dtypes(include=[object, pd.datetime]).columns
+        
+        if num_cols.values.shape[0] > 0:
+            pipe_num = Pipeline(
+            steps = [
+                ("selector_num", ColumnTransformer([("selector", "passthrough", num_cols.values)], remainder = 'drop')),
+                ('num_imputer', SimpleImputer(strategy='mean'))
+            ])
+        else:
+            pipe_num = None
+            
+        if cat_cols.values.shape[0] > 0:
+            pipe_cat = Pipeline(
+            steps = [
+                ("selector_cat", ColumnTransformer([("selector", "passthrough", cat_cols.values)], remainder = 'drop')),
+                ("OrdinalEnc", OrdinalEncoder(handle_unknown = "use_encoded_value", unknown_value = -1)),
+                ('cat_imputer', SimpleImputer(strategy='constant', fill_value='None'))
+            ])        
+        else:
+            pipe_cat = None
+        
+        if pipe_num is not None and pipe_cat is not None:
+            prep_feat = FeatureUnion(
+            transformer_list = [
+                ('num_pipe', pipe_num),
+                ('cat_pipe', pipe_cat)
+                
+            ], verbose = False)  
+            
+            return (prep_feat, num_cols.values, cat_cols.values)
+
+        if pipe_num is not None and pipe_cat is None:
+            prep_feat = FeatureUnion(
+            transformer_list = [
+                ('num_pipe', pipe_num)
+                
+            ], verbose = False)
+            
+            return (prep_feat, num_cols.values, [])
+
+        if pipe_num is None and pipe_cat is not None:
+            prep_feat = FeatureUnion(
+            transformer_list = [
+                ('cat_pipe', pipe_cat)
+                
+            ], verbose = False) 
+            
+            return (prep_feat, [], cat_cols.values)
+
+
+    def plot_dist(y_train, pred_proba_train, y_val, pred_proba_val):
+        
+        fig, axs = plt.subplots(nrows = 1, ncols = 2, figsize = (16,6))
+        plt.subplots_adjust(left = None, right = None, top = None, bottom = None, wspace = 0.2, hspace = 0.4)
+        
+        vis = pd.DataFrame()
+        vis['target'] = y_train
+        vis['proba'] = pred_proba_train
+        
+        list_1 = vis[vis.target == 1].proba
+        list_2 = vis[vis.target == 0].proba
+        
+        sns.distplot(list_1, kde = True, ax = axs[0], hist = True, bins = 100)
+        sns.distplot(list_2, kde = True, ax = axs[0], hist = True, bins = 100)
+        
+        axs[0].set_title('train Thereshold Curve')
+        
+        
+        
+        vis = pd.DataFrame()
+        vis['target'] = y_val
+        vis['proba'] = pred_proba_val
+        
+        list_1 = vis[vis.target == 1].proba
+        list_2 = vis[vis.target == 0].proba
+        
+        sns.distplot(list_1, kde = True, ax = axs[1], hist = True, bins = 100)
+        sns.distplot(list_2, kde = True, ax = axs[1], hist = True, bins = 100)
+        
+        axs[1].set_title('valid Thereshold Curve')    
